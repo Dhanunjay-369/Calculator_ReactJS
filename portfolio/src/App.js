68ccf0820064e815da99ac088f515244cd927a56
Dhanunjay <dhanunjayareddykurakula@gmail.com>
2023-10-14 23:35:53 +0530

    first commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import {HashRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
-import Main from './components/MainPage.js';
+import {Main} from './components/MainPage.js';
+import {About} from './components/AboutPage.js';
 function App() {
   return (
     <div className="App">
       <Main/>
+      <About/>
     </div>
   );
 }
